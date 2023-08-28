fc76c8608f0d72ecf7a1bfde3e8758c5b2f4bcf2
Minh Nguyen <minhnguyen@mcps.org>
2023-08-28 19:23:11 -0400

    started wrist!

---
+++ src/main/java/frc/robot/commands/arm/WristMove.java
@@ -0,0 +1,16 @@
+package frc.robot.commands.arm;
+
+import frc.robot.subsystems.arm.WristSubsystem;
+
+public class WristMove {
+
+    private WristSubsystem wrist;
+
+    public WristMove(WristSubsystem wrist) {
+        this.wrist = wrist;
+    }
+
+
+
+    $ git config --global user.name "Minh Nguyen"
+}
---
+++ src/main/java/frc/robot/subsystems/arm/WristSubsystem.java
@@ -0,0 +1,31 @@
+package frc.robot.subsystems.arm;
+
+import com.ctre.phoenix.motorcontrol.NeutralMode;
+import com.ctre.phoenix.motorcontrol.can.TalonFX;
+import com.revrobotics.AbsoluteEncoder;
+import com.revrobotics.CANEncoder;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.RelativeEncoder;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
+import edu.wpi.first.wpilibj.CAN;
+
+
+public class WristSubsystem {
+    
+    private CANSparkMax wristMotor;
+
+    public WristSubsystem() {
+        wristMotor = new CANSparkMax(0, MotorType.kBrushless);
+
+    }
+
+    public double getPositionRad() {
+        return wristMotor.getEncoder().getPosition();
+    }
+
+    public void setMotorPower(double speed) {
+        wristMotor.set(speed);
+    }
+
+}
