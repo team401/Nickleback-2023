5403303cef90f18aff4b874549078ad85d09ecaa
Minh Nguyen <minhnguyen@mcps.org>
2023-08-30 19:41:41 -0400

    wrist (+maybe shooter?) combined into arm subsys

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -21,12 +21,12 @@ public class RobotContainer {
 =======
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
-import frc.robot.subsystems.arm.WristSubsystem;
+import frc.robot.subsystems.arm.ArmSubsystem;
 
 public class RobotContainer {
 
 
-    private WristSubsystem wrist = new WristSubsystem();
+    private ArmSubsystem wrist = new ArmSubsystem();
     private XboxController gamepad = new XboxController(0);
 
     public RobotContainer() {
