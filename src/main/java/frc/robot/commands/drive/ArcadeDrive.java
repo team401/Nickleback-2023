7593382f6b3077738872328c9f0327786b324e81
wrob3rts <willenderroberts@gmail.com>
2023-09-06 18:34:00 -0400

    added arcade drive (functionality unkown)

---
+++ src/main/java/frc/robot/commands/drive/ArcadeDrive.java
@@ -0,0 +1,30 @@
+package frc.robot.commands.drive;
+
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.DriveSubsystem;
+
+public class ArcadeDrive extends CommandBase{
+    
+    private final DriveSubsystem drivesub;
+    private final Joystick l_Joystick;
+    private final Joystick r_Joystick;
+
+    public ArcadeDrive(DriveSubsystem d, Joystick l, Joystick r) {
+        drivesub = d;
+        l_Joystick = l;
+        r_Joystick = r;
+        addRequirements(drivesub);
+}
+
+@Override
+public void initialize() {
+}
+@Override
+public void execute() {
+    drivesub.arcadeDrive(l_Joystick.getX(), r_Joystick.getY());
+}
+@Override
+public void end(boolean interrupted){
+}
+}
