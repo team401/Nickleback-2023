248a8682bb83d55f6802cc548a2a1ec51c95b8d8
Minh Nguyen <minhnguyen@mcps.org>
2023-09-06 18:41:50 -0400

    constants?

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -15,6 +15,14 @@ public class Constants {
 }
 =======
     
-    
+    public final class ArmConstants {
+
+        public static final int wristMotorID = 0;
+        public static final int leftMotorID = 1;
+        public static final int rightMotorID = 2;
+        public static final int intakeMotorID = 3;
+
+    }
+
 }
 >>>>>>> 64ff558 (started wrist code!)
--- src/main/java/frc/robot/commands/arm/ArmMove.java
+++ src/main/java/frc/robot/commands/arm/ArmMove.java
@@ -1,6 +1,5 @@
 package frc.robot.commands.arm;
 
-import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystems.arm.ArmSubsystem;
@@ -9,18 +8,27 @@ public class ArmMove extends CommandBase {
 
     private ArmSubsystem arm;
     private double wristGoalRad, shooterPowerGoal;
+    private boolean intakeMotorStatus;
 
-    public ArmMove (ArmSubsystem arm, double wristGoalRad, double shooterPowerGoal) {
+    public ArmMove (ArmSubsystem arm, double wristGoalRad, double shooterPowerGoal, boolean intakeMotorStatus) {
         this.arm = arm;
         this.wristGoalRad = wristGoalRad;
         this.shooterPowerGoal = shooterPowerGoal;
+        this.intakeMotorStatus = intakeMotorStatus;
     }
 
     @Override
     public void initialize() {
         wristGoalRad = SmartDashboard.getNumber("wrist goal rad", wristGoalRad);
         shooterPowerGoal = SmartDashboard.getNumber("shooter power goal", shooterPowerGoal);
-        arm.setWristGoalRads(wristGoalRad); arm.setShooterGoalPower(shooterPowerGoal);
+        intakeMotorStatus = SmartDashboard.getBoolean("intake motor activated", intakeMotorStatus);
+        arm.setWristGoalRads(wristGoalRad); arm.setShooterGoalPower(shooterPowerGoal); 
+    }
+
+    @Override
+    public boolean isFinished() {
+        // TODO Auto-generated method stub
+        return super.isFinished();
     }
 
 
