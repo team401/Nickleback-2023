ae466d0715b1dc62fa381ffd42d2d011e98a96b3
GideonC <100504528+TheLumpiest@users.noreply.github.com>
2023-09-06 19:33:05 -0400

    Drive commands (#11)
    * getting started on the drive train
    
    * Created TandDrive command and edited DriveSubsystem
    
    * test
    
    * edited DriveSubsystem
    
    * added arcade drive (functionality unkown)
    
    * Fixed a typo
    
    * formatting
    
    * Also Formatting
    
    * More Formatting
    
    * formatting
    
    ---------
    
    Co-authored-by: Honzik <redploveryt@gmail.com>
    Co-authored-by: Aethers Haven <sullivandovie@gmail.com>
    Co-authored-by: wrob3rts <willenderroberts@gmail.com>

---
+++ src/main/java/frc/robot/subsystems/DriveSubsystem.java
@@ -0,0 +1,92 @@
+package frc.robot.subsystems;
+
+import com.ctre.phoenix.motorcontrol.NeutralMode;
+import com.ctre.phoenix.motorcontrol.can.TalonFX;
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
+import com.ctre.phoenix.sensors.AbsoluteSensorRange;
+import com.ctre.phoenix.sensors.CANCoder;
+import com.revrobotics.CANSparkMax.IdleMode;
+import com.ctre.phoenix.sensors.PigeonIMU;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.math.MathUtil;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.kinematics.SwerveModuleState;
+import edu.wpi.first.math.util.Units;
+import edu.wpi.first.wpilibj.motorcontrol.MotorController;
+import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
+import frc.robot.Constants.DriveConstants;
+
+public class DriveSubsystem extends SubsystemBase{
+
+    private final WPI_TalonFX driveMotorFrontLeft;
+    private final WPI_TalonFX driveMotorFrontRight;
+    private final WPI_TalonFX driveMotorBackLeft;
+    private final WPI_TalonFX driveMotorBackRight;
+    private final MotorControllerGroup rightMotorControllerGroup;
+    private final MotorControllerGroup leftMotorControllerGroup;
+    private final DifferentialDrive drive;
+	private final PigeonIMU pigeon;
+    private final double angleOffset = 0;
+    private enum DriveMode{
+        TANKDRIVE,
+        ARCADEDRIVE
+    }
+
+    private DriveMode mode = DriveMode.TANKDRIVE;
+    private double leftTank = 0;
+    private double rightTank = 0;
+    private double arcadeForward = 0;
+    private double arcadeRotation = 0;
+
+    public DriveSubsystem(){
+        driveMotorFrontLeft = new WPI_TalonFX(Constants.DriveConstants.frontLeftDriveMotorID);
+        driveMotorFrontRight = new WPI_TalonFX(Constants.DriveConstants.frontRightDriveMotorID);
+        driveMotorBackLeft = new WPI_TalonFX(Constants.DriveConstants.backLeftDriveMotorID);
+        driveMotorBackRight = new WPI_TalonFX(Constants.DriveConstants.backRightDriveMotorID);
+        driveMotorFrontLeft.setNeutralMode(NeutralMode.Coast);
+        driveMotorFrontRight.setNeutralMode(NeutralMode.Coast);
+        driveMotorBackLeft.setNeutralMode(NeutralMode.Coast);
+        driveMotorBackRight.setNeutralMode(NeutralMode.Coast);
+        rightMotorControllerGroup = new MotorControllerGroup(driveMotorFrontRight, driveMotorBackRight);
+        leftMotorControllerGroup = new MotorControllerGroup(driveMotorFrontLeft, driveMotorBackLeft);
+        drive = new DifferentialDrive(leftMotorControllerGroup, rightMotorControllerGroup);
+        pigeon = new PigeonIMU(DriveConstants.pigeonID);
+    }
+
+    public double GetDriveAngle()
+	{
+		return pigeon.getYaw()-angleOffset;
+	}
+
+    public void setArcadeDriveControls(double forward, double rotation) {
+		arcadeForward = forward;
+        arcadeRotation = rotation;
+	}
+
+    public void arcadeDrive(){
+        drive.arcadeDrive(arcadeForward, -arcadeRotation);
+    }
+
+	public void setTankDriveControls(double left, double right) {
+        leftTank = left;
+        rightTank = right;
+	}
+
+    public void tankDrive(){
+        drive.tankDrive(leftTank, rightTank);
+    }
+
+    @Override
+    public void periodic(){
+        if(mode == DriveMode.TANKDRIVE){
+            tankDrive();
+        }
+        else{
+            arcadeDrive();
+        }
+    } 
+
+}
