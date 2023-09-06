f80c80aab6dd98f4d227f2f81b55451db1ad7d4c
Minh Nguyen <minhnguyen@mcps.org>
2023-09-06 18:41:50 -0400

    constants?

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -2,5 +2,13 @@ package frc.robot;
 
 public class Constants {
     
-    
+    public final class ArmConstants {
+
+        public static final int wristMotorID = 0;
+        public static final int leftMotorID = 1;
+        public static final int rightMotorID = 2;
+        public static final int intakeMotorID = 3;
+
+    }
+
 }
