3ea2a50243543cf097e682cd3af531948ed2921d
Bread-n-Butter06 <emilypaul@new-school.org>
2023-09-06 19:39:45 -0400

    start button bindings

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -1,21 +1,62 @@
 package frc.robot;
 
 import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
-import frc.robot.subsystems.arm.ArmSubsystem;
+import frc.robot.subsystems.arm.ArmSubsystem;import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
+import edu.wpi.first.wpilibj2.command.ParallelRaceGroup;
+import edu.wpi.first.wpilibj2.command.RunCommand;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import edu.wpi.first.wpilibj2.command.WaitCommand;
+import edu.wpi.first.wpilibj2.command.WaitUntilCommand;
+import edu.wpi.first.wpilibj2.command.button.JoystickButton;
+import frc.robot.ButtonMasher;
+import frc.robot.Thrustmaster;
 
 public class RobotContainer {
 
-
+    
     private ArmSubsystem wrist = new ArmSubsystem();
-    private XboxController gamepad = new XboxController(0);
+    //private XboxController gamepad = new XboxController(0);
+
+    private final Thrustmaster leftStick = new Thrustmaster(0);
+    private final Thrustmaster rightStick = new Thrustmaster(1);
+    private final ButtonMasher masher = new ButtonMasher(2);
+
+    SendableChooser<String> autoChooser = new SendableChooser<String>();
+    private Command activeAutoCommand = null;
+    private String activeAutoName = null;
+    }
 
     public RobotContainer() {
-        configureButtonBindings();
+
+        configureSubsystems();
+        configureCompBindings();
+        // configureTestBindings();
     }
 
-    private void configureButtonBindings() {
+    private void configureCompBindings() {
 
     }
 
+    private void configureSubsystems() {
+
+        drive.setDefaultCommand(new DriveWithJoysticks(
+            drive,
+            () -> -leftStick.getRawAxis(1),
+            () -> -leftStick.getRawAxis(0),
+            () -> -rightStick.getRawAxis(0),
+            true
+        ));
+
+    }
 }
