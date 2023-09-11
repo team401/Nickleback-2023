918a86ac65b4ff930688a50474f95f1fd74649fd
Minh Nguyen <minhnguyen@mcps.org>
2023-09-11 19:44:45 -0400

    drive maaybe done needs verification by justin

---
+++ src/main/java/frc/robot/subsystems/DriveHardware.java
@@ -0,0 +1,32 @@
+package frc.robot.subsystems;
+
+import com.ctre.phoenix.sensors.PigeonIMU;
+
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
+import frc.robot.Constants.DriveConstants;
+
+public class DriveHardware {
+
+    protected MotorControllerGroup rightMotorControllerGroup;
+    protected MotorControllerGroup leftMotorControllerGroup;
+    protected DifferentialDrive drive;
+	protected final PigeonIMU pigeon = new PigeonIMU(DriveConstants.pigeonID);
+    protected final double angleOffset = 0;
+
+    public DriveHardware() {}
+    
+    public double GetDriveAngle() {
+		return pigeon.getYaw()-angleOffset;
+	}
+
+    public void tankDrive(double leftTank, double rightTank) {
+        drive.tankDrive(leftTank, rightTank);
+    }
+
+    public void arcadeDrive(double forward, double rotation) {
+        drive.arcadeDrive(forward, -rotation);
+    }
+   
+
+}
