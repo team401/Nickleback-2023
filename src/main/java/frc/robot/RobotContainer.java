40cc72823d959f1e091e79336352e83c3f6cccf2
TheLumpiest <gideon.colliver@gmail.com>
2023-09-13 18:32:04 -0500

    fixed robot container

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -15,27 +15,15 @@ import edu.wpi.first.wpilibj2.command.button.Trigger;
 import frc.robot.commands.drive.ArcadeDrive;
 import frc.robot.commands.drive.TankDrive;
 import frc.robot.subsystems.DriveSubsystem;
-import frc.robot.subsystems.arm.ArmSubsystem;
+import frc.robot.subsystems.ArmSubsystem;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
-import edu.wpi.first.wpilibj2.command.button.JoystickButton;
-import frc.robot.subsystems.arm.ArmSubsystem;import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.DriverStation;
-import edu.wpi.first.wpilibj.Joystick;
-import edu.wpi.first.wpilibj.XboxController;
-import edu.wpi.first.wpilibj.XboxController.Button;
-import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
-import edu.wpi.first.wpilibj2.command.RunCommand;
-import edu.wpi.first.wpilibj2.command.button.JoystickButton;
-import edu.wpi.first.wpilibj2.command.button.POVButton;
-import edu.wpi.first.wpilibj2.command.button.Trigger;
 import frc.robot.Constants.ArmConstants;
 import frc.robot.Constants.DriveConstants;
 import frc.robot.commands.ArmMove;
 import frc.robot.commands.Intake;
-import frc.robot.subsystems.ArmSubsystem;
 
 public class RobotContainer extends SubsystemBase{
     private final DriveSubsystem drive = new DriveSubsystem();
@@ -43,8 +31,8 @@ public class RobotContainer extends SubsystemBase{
     private final Joystick leftStick = new Joystick(0);
     private final Joystick rightStick = new Joystick(1);
     private final XboxController gamepad = new XboxController(2);
-
     private ArmSubsystem wrist = new ArmSubsystem();
+    private Intake intake = new Intake();
 
     public RobotContainer(){
 
@@ -54,26 +42,11 @@ public class RobotContainer extends SubsystemBase{
 
     }
 
-
-    private final Joystick leftStick = new Joystick(0);
-    private final Joystick rightStick = new Joystick(1);
-    private final XboxController gamepad = new XboxController(2);
-    private ArmSubsystem wrist = new ArmSubsystem();
-    private Intake intake = new Intake();
-
     //private XboxController gamepad = new XboxController(0);
 
     SendableChooser<String> autoChooser = new SendableChooser<String>();
     private Command activeAutoCommand = null;
     private String activeAutoName = null;
-    }
-
-    public RobotContainer() {
-        drive.setDefaultCommand(new OperatorControl(drive,
-                () -> leftStick.getRawAxis(1),
-                () -> rightStick.getRawAxis(0)));
-        configureButtonBindings();
-        
     }
 
     private void configureButtonBindings() {
@@ -87,8 +60,5 @@ public class RobotContainer extends SubsystemBase{
     }
 
     private void configureSubsystems() {
-
-        
-
     }
 }
