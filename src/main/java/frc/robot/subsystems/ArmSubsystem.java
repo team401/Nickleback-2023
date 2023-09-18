0596ebc5f53a899b326181c7e7b2d76883c0a3bc
Minh Nguyen <minhnguyen@mcps.org>
2023-09-18 19:50:27 -0400

    issue27 done except for voltage testing+not tested

--- src/main/java/frc/robot/subsystems/ArmSubsystem.java
+++ src/main/java/frc/robot/subsystems/ArmSubsystem.java
@@ -1,9 +1,14 @@
 package frc.robot.subsystems;
 
+import java.util.concurrent.ThreadPoolExecutor;
+
 import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMax.IdleMode;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
+import edu.wpi.first.math.controller.ArmFeedforward;
 import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.trajectory.TrapezoidProfile;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
@@ -13,19 +18,19 @@ import frc.robot.Constants;
 public class ArmSubsystem extends SubsystemBase{
     
     private CANSparkMax wristMotor;
-    private double wristkp = 0, wristki = 0, wristkd = 0;
-    private double wristTolerance = 0.01;
+    private double wristkp = 0, wristki = 0, wristkd = 0, wristkg = 0, wristkv = 0, wristks = 0;
+    private final double wristTolerance = 0.01;
     private PIDController wristController = new PIDController(wristkp, wristki, wristkd);
+    private ArmFeedforward feedforward = new ArmFeedforward(wristks, wristkg, wristkv);
     private static double wristGoalRad = 0;
-    private final double softStop = 80.0, hardStop = 100.0;
+    private final double softStop = 30.0, hardStop = 40.0;
 
     private CANSparkMax leftMotor, rightMotor, intakeMotor;
     private double shooterGoalPower;
     private double intakeGoalPower;
 
-
-    private double wristIntake, wristShootHigh, wristShootMid, wristShootLow, wristIdle;
-    private double shooterIntake, shooterShootHigh, shooterShootMid, shooterShootLow, shooterIdle = 0;
+    private double wristIntake, wristShootHigh, wristShootMid, wristShootLow, wristWarmup;
+    private double shooterIntake, shooterShootHigh, shooterShootMid, shooterShootLow, shooterWarmup = 0;
     private double intakeOn, intakeOff = 0;
 
     public static enum ArmPositions {
@@ -33,10 +38,11 @@ public class ArmSubsystem extends SubsystemBase{
         ShootHigh,
         ShootMid,
         ShootLow,
+        Warmup,
         Idle;
     }
 
-    private ArmPositions currentArmPos = ArmPositions.Idle;
+    private ArmPositions currentArmPos;
 
     public ArmSubsystem() {
         wristMotor = new CANSparkMax(Constants.ArmConstants.wristMotorID, MotorType.kBrushless);
@@ -44,7 +50,12 @@ public class ArmSubsystem extends SubsystemBase{
         leftMotor = new CANSparkMax(Constants.ArmConstants.leftMotorID, MotorType.kBrushless);
         rightMotor = new CANSparkMax(Constants.ArmConstants.rightMotorID, MotorType.kBrushless);
         leftMotor.follow(rightMotor, true);
+        wristMotor.setIdleMode(IdleMode.kBrake);
+        intakeMotor.setIdleMode(IdleMode.kBrake);
+        leftMotor.setIdleMode(IdleMode.kBrake);
+        rightMotor.setIdleMode(IdleMode.kBrake);
 
+        setMode(ArmPositions.Warmup);
     }
     
     public void setMode(ArmPositions armPos) {
@@ -66,9 +77,9 @@ public class ArmSubsystem extends SubsystemBase{
             wristGoalRad = wristShootLow;
             shooterGoalPower = shooterShootLow;
             intakeGoalPower = intakeOff;
-        } else if (armPos == ArmPositions.Idle) {
-            wristGoalRad = wristIdle;
-            shooterGoalPower = shooterIdle;
+        } else if (armPos == ArmPositions.Warmup) {
+            wristGoalRad = wristWarmup;
+            shooterGoalPower = shooterWarmup;
             intakeGoalPower = intakeOff;
         }
         
@@ -81,7 +92,9 @@ public class ArmSubsystem extends SubsystemBase{
     ////////////////////////////////////////////////////////////////////////////////////////////////////
 
     public double getWristPositionRad() {
-        return wristMotor.getEncoder().getPosition() * 2 * Math.PI;
+        return wristMotor.getEncoder().getPosition() * 2 * Math.PI / 10; 
+        // ???? check gear ratios again because I don't know if I calculated them right
+        // from Abby: 10:1, 3:1, 16:48
     }
 
     public void setWristMotorPower(double percent) {
@@ -95,19 +108,24 @@ public class ArmSubsystem extends SubsystemBase{
     private void checkWristAmps() {
         if (getWristMotorAmps() > softStop) {
             wristGoalRad = getWristPositionRad();
+            if (getWristMotorAmps() > hardStop) {
+                setWristMotorPower(0);
+                currentArmPos = ArmPositions.Idle;
+            }
         } 
-        if (getWristMotorAmps() > hardStop) {
-            setWristMotorPower(0);
-        }
+        
     }
-
 
     private void wristControl() {
         // add TrapezoidProfile + feedforward later?
-        double start = getWristPositionRad();
-        double x = wristController.calculate(wristGoalRad, start);
-        setWristMotorPower(x);
-        checkWristAmps();
+        if (currentArmPos != ArmPositions.Idle) {
+            double start = getWristPositionRad();
+            double x = wristController.calculate(wristGoalRad, start) + feedforward.calculate(wristGoalRad, 0);
+            setWristMotorPower(x);
+            checkWristAmps();
+        } else {
+            setWristMotorPower(0);
+        }
     }
 
     public boolean wristFinished() {
@@ -129,14 +147,17 @@ public class ArmSubsystem extends SubsystemBase{
         if (getShooterMotorAmps() > hardStop) {
             shooterGoalPower = 0;
             setShooterMotorPower(0);
- 
+            currentArmPos = ArmPositions.Idle;
         }
     }
 
-
     public void shooterControl() {
-        setShooterMotorPower(shooterGoalPower);
-        checkShooterAmps();
+        if (currentArmPos != ArmPositions.Idle) {
+            setShooterMotorPower(shooterGoalPower);
+            checkShooterAmps();
+        } else {
+            setShooterMotorPower(0);
+        }
     }
 
     //////////////////////////////////////////////////////////////////////////////////////////////
@@ -153,7 +174,7 @@ public class ArmSubsystem extends SubsystemBase{
         if (getIntakeMotorAmps() > hardStop) {
             intakeGoalPower = 0;
             setIntakeMotorPower(0);
- 
+            currentArmPos = ArmPositions.Idle;
         }
     }
 
@@ -162,8 +183,12 @@ public class ArmSubsystem extends SubsystemBase{
     }
 
     public void intakeControl() {
-        setIntakeGoalPower(intakeGoalPower);
-        checkIntakeAmps();
+        if (currentArmPos != ArmPositions.Idle) {
+            setIntakeGoalPower(intakeGoalPower);
+            checkIntakeAmps();
+        } else {
+            setIntakeGoalPower(0);
+        }
     }
 
 
@@ -177,22 +202,29 @@ public class ArmSubsystem extends SubsystemBase{
         wristkp = SmartDashboard.getNumber("wrist kp", 0);
         wristki = SmartDashboard.getNumber("wrist ki", 0);
         wristkd = SmartDashboard.getNumber("wrist kd", 0);
+        wristks = SmartDashboard.getNumber("wrist ks", 0);
+        wristkg = SmartDashboard.getNumber("wrist kg", 0);
+        wristkv = SmartDashboard.getNumber("wrist kv", 0);
+
+        SmartDashboard.putNumber("shooter amps", getShooterMotorAmps());
+        SmartDashboard.putNumber("intake amps", getIntakeMotorAmps());
 
         wristIntake = SmartDashboard.getNumber("wrist intake rad", 0);
         wristShootHigh = SmartDashboard.getNumber("wrist shoot high rad", 0);
         wristShootMid = SmartDashboard.getNumber("wrist shoot mid rad", 0);
         wristShootLow = SmartDashboard.getNumber("wrist shoot low rad", 0);
-        wristIdle = SmartDashboard.getNumber("wrist shoot idle rad", 0);
+        wristWarmup = SmartDashboard.getNumber("wrist shoot idle rad", 0);
 
         shooterIntake = SmartDashboard.getNumber("shooter intake power", 0);
         shooterShootHigh = SmartDashboard.getNumber("shooter shoot high power", 0);
         shooterShootMid = SmartDashboard.getNumber("shooter shoot mid power", 0);
         shooterShootLow = SmartDashboard.getNumber("shooter shoot low power", 0);
-        shooterIdle = SmartDashboard.getNumber("shooter idle power", 0);
+        shooterWarmup = SmartDashboard.getNumber("shooter idle power", 0);
 
         intakeOn = SmartDashboard.getNumber("intake power", 0);
 
 
+        
         wristControl();   
         
         if(wristFinished()) {
