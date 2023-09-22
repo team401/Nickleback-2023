e4ea438b4877e87f5b9975ef4efcafdbe0c00cc0
William Roberts <willenderroberts@gmail.com>
2023-09-21 21:18:06 -0400

    Controller configuration (#32)
    * intake subsystem file created
    
    * Updated everything to use gamepad and drive swap
    
    ---------
    
    Co-authored-by: GideonC <gideon.colliver@gmail.com>
    Co-authored-by: redplover <redploveryt@gmail.com>

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -6,10 +6,14 @@ import edu.wpi.first.wpilibj.XboxController.Button;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
+import frc.robot.commands.drive.ArcadeDrive;
 import frc.robot.commands.drive.TankDrive;
+import frc.robot.subsystems.drive.DriveSubsystem;
 import frc.robot.subsystems.ArmSubsystem;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import frc.robot.commands.Intake;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
@@ -28,7 +32,7 @@ public class RobotContainer {
 
     private final Joystick leftStick = new Joystick(0);
     private final Joystick rightStick = new Joystick(1);
-    private final XboxController gamepad = new XboxController(2);
+    private final CommandXboxController gamepad = new CommandXboxController(2);
     private ArmSubsystem wrist = new ArmSubsystem();
     private Intake intake = new Intake();
 
@@ -61,15 +65,28 @@ public class RobotContainer {
     }
 
     private void configureButtonBindings() {
-        new JoystickButton(gamepad, Button.kY.value)
+        gamepad.x()
             .onTrue(new InstantCommand(intake::shoot))
             .onFalse(new InstantCommand(intake::stop));
-        new JoystickButton(gamepad, Button.kX.value)
+        gamepad.y()
             .onTrue(new InstantCommand(intake::spit))
             .onFalse(new InstantCommand(intake::stop));
-            
+        
+        gamepad.a()
+            .onTrue(new InstantCommand(() -> swapDrive()));
     }
 
-    private void configureSubsystems() {
+    private void swapDrive() {
+        drive.swapMode();
+        if (drive.getMode() == DriveMode.TANKDRIVE) {
+            drive.setDefaultCommand(new TankDrive(drive,
+                () -> gamepad.getLeftY(),
+                () -> gamepad.getRightY()));
+        }
+        else {
+            drive.setDefaultCommand(new ArcadeDrive(drive, 
+                () -> gamepad.getLeftY(), 
+                () -> gamepad.getRightX()));
+        }
     }
 }
