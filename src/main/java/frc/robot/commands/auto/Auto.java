66330541d2b877ddcfea7504fb71f480236aaef7
fujibookpro <maczone06@gmail.com>
2023-09-28 23:06:37 -0400

    Fixed auto command to match arm changes

--- src/main/java/frc/robot/commands/auto/Auto.java
+++ src/main/java/frc/robot/commands/auto/Auto.java
@@ -10,7 +10,7 @@ import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.WaitCommand;
 import frc.robot.commands.ArmMove;
 import frc.robot.subsystems.ArmSubsystem;
-import frc.robot.subsystems.ArmSubsystem.ArmPositions;
+import frc.robot.subsystems.ArmSubsystem.Mode;
 import frc.robot.subsystems.drive.DriveSubsystem;
 
 public class Auto extends SequentialCommandGroup {
@@ -19,12 +19,9 @@ public class Auto extends SequentialCommandGroup {
     public Auto(ArmSubsystem arm, DriveSubsystem drive, double driveTime) {
 
         addCommands(
-            new InstantCommand(() -> arm.setMode(ArmPositions.ShootHigh)),
-            new WaitCommand(0.5)
-                .andThen(new InstantCommand(arm::shoot)),
-            new WaitCommand(0.5)
-                .andThen(new InstantCommand(arm::stopIntake))
-                .andThen(new InstantCommand(() -> arm.setMode(ArmPositions.Idle))),
+            new InstantCommand(() -> arm.setMode(Mode.SHOOT_HIGH)),
+            new WaitCommand(1),
+            new InstantCommand(() -> arm.setMode(Mode.IDLE)),
             new ParallelRaceGroup(
                 new WaitCommand(driveTime),
                 new InstantCommand(() -> drive.setTankDriveControls(1,1))
