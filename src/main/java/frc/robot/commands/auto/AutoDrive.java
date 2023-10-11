a0f9b55eaa3a3e09c489572cf7028b740479df63
Minh Nguyen <minhnguyen@mcps.org>
2023-10-11 18:46:47 -0400

    waiting on swerve and odometry :D

--- src/main/java/frc/robot/commands/auto/AutoDrive.java
+++ src/main/java/frc/robot/commands/auto/AutoDrive.java
@@ -5,15 +5,11 @@ import com.pathplanner.lib.controllers.PPHolonomicDriveController;
 
 import edu.wpi.first.math.controller.HolonomicDriveController;
 import edu.wpi.first.math.controller.PIDController;
-import edu.wpi.first.math.controller.ProfiledPIDController;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
-import edu.wpi.first.math.trajectory.Trajectory;
-import edu.wpi.first.math.trajectory.TrapezoidProfile;
-import edu.wpi.first.math.trajectory.TrapezoidProfile.State;
-import edu.wpi.first.wpilibj.AddressableLED;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystems.drive.DriveSubsystem;
 
@@ -21,8 +17,12 @@ public class AutoDrive extends CommandBase {
 
     private DriveSubsystem drive;
     private PathPlannerTrajectory path;
+
     private PIDController xController, yController;
     private PIDController thetaController;
+
+    private double kPX = 0, kIX = 0, kDX = 0, kPY = 0, kIY = 0, kDY = 0, kPTheta = 0, kITheta = 0, kDTheta = 0;
+
     private PPHolonomicDriveController controller;
     private DriverStation.Alliance alliance;
 
@@ -35,9 +35,9 @@ public class AutoDrive extends CommandBase {
         this.drive = drive;
         this.path = PathPlannerTrajectory.transformTrajectoryForAlliance(path, alliance);
 
-        xController = new PIDController(0, 0, 0);
-        yController = new PIDController(0, 0, 0);
-        thetaController = new PIDController(0, 0, 0);
+        xController = new PIDController(kPX, kIX, kPX);
+        yController = new PIDController(kPY, kIY, kDY);
+        thetaController = new PIDController(kPTheta, kITheta, kDTheta);
         thetaController.enableContinuousInput(0, 2*Math.PI);
 
         controller = new PPHolonomicDriveController(xController, yController, thetaController);
@@ -50,17 +50,42 @@ public class AutoDrive extends CommandBase {
     public void initialize() {
         time.reset();
         time.start();  
+
+        SmartDashboard.putNumber("kP X", kPX);
+        SmartDashboard.putNumber("kI X", kIX);
+        SmartDashboard.putNumber("kD X", kDX);
+
+        SmartDashboard.putNumber("kP Y", kPY);
+        SmartDashboard.putNumber("kI Y", kIY);
+        SmartDashboard.putNumber("kD Y", kDY);
+
+        SmartDashboard.putNumber("kP Theta", kPTheta);
+        SmartDashboard.putNumber("kI Theta", kITheta);
+        SmartDashboard.putNumber("kD Theta", kDTheta);
     }
 
     @Override 
     public void execute() {
 
         PathPlannerTrajectory.PathPlannerState goal = ((PathPlannerTrajectory.PathPlannerState) path.sample(time.get()));
-        Pose2d currentRobotPose = new Pose2d(); // get from odometry
+        Pose2d currentRobotPose = new Pose2d(); 
+        //TODO: get from odometry
         
         ChassisSpeeds adjustedSpeeds = controller.calculate(currentRobotPose, goal);
 
-        //return to drive???
+        SmartDashboard.getNumber("kP X", kPX);
+        SmartDashboard.getNumber("kI X", kIX);
+        SmartDashboard.getNumber("kD X", kDX);
+
+        SmartDashboard.getNumber("kP Y", kPY);
+        SmartDashboard.getNumber("kI Y", kIY);
+        SmartDashboard.getNumber("kD Y", kDY);
+
+        SmartDashboard.getNumber("kP Theta", kPTheta);
+        SmartDashboard.getNumber("kI Theta", kITheta);
+        SmartDashboard.getNumber("kD Theta", kDTheta);
+
+        //TODO: return to drive???
     }
 
     @Override
--- src/main/java/frc/robot/commands/auto/Balance.java
+++ src/main/java/frc/robot/commands/auto/Balance.java
@@ -1,31 +1,35 @@
 package frc.robot.commands.auto;
 
 import com.ctre.phoenix.sensors.Pigeon2;
+import com.ctre.phoenix.sensors.PigeonIMU;
 
 import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystems.drive.DriveSubsystem;
 
 public class Balance extends CommandBase {
 
     private DriveSubsystem drive;
-    private Pigeon2 pigeon = new Pigeon2(0);
+    private PigeonIMU pigeon = new PigeonIMU(0); //TODO: get pigeon
 
     private PIDController driveController, balanceController;
 
-    private double targetStationX = 0;
+    private double targetStationX = 0; //TODO: get location of targetStationX
 
     private Timer searchTimer = new Timer();
     private Timer balanceTimer = new Timer();
 
     private boolean balanceFound;
 
+    private double kPDrive = 0, kIDrive = 0, kDDrive = 0, kPBalance = 0, kIBalance = 0, kDBalance = 0;
+
     public Balance(DriveSubsystem drive) {
         this.drive = drive;
-        driveController = new PIDController(0, 0, 0);
-        balanceController = new PIDController(0, 0, 0);
+        driveController = new PIDController(kPDrive, kIDrive, kDDrive);
+        balanceController = new PIDController(kPBalance, kIBalance, kDBalance);
 
     }
 
@@ -36,15 +40,23 @@ public class Balance extends CommandBase {
 
         balanceFound = false;
 
+        SmartDashboard.putNumber("kP Drive", kPDrive);
+        SmartDashboard.putNumber("kI Drive", kIDrive);
+        SmartDashboard.putNumber("kD Drive", kDDrive);
+
+        SmartDashboard.putNumber("kP Balance", kPBalance);
+        SmartDashboard.putNumber("kI Balance", kIBalance);
+        SmartDashboard.putNumber("kD Balance", kDBalance);
+
     }
 
     @Override
     public void execute() {
         if (!balanceFound) {
-            Pose2d currentPose = new Pose2d(); //get current pos
+            Pose2d currentPose = new Pose2d(); //TODO: get current pos
             double currentX = currentPose.getX();
             double calculate = driveController.calculate(currentX, targetStationX);
-            //apply power to wheels 
+            //TODO: apply power to wheels 
             if (Math.abs(pigeon.getRoll()) < 0.2) {
                 balanceFound = true;
                 searchTimer.stop();
@@ -54,8 +66,19 @@ public class Balance extends CommandBase {
         } else {
             double pitch = pigeon.getRoll();
             double dir = balanceController.calculate(pitch, 0);
-            //apply dir to swerve
+            //TODO: apply dir to swerve
         }
+
+
+    
+        SmartDashboard.getNumber("kP Drive", kPDrive);
+        SmartDashboard.getNumber("kI Drive", kIDrive);
+        SmartDashboard.getNumber("kD Drive", kDDrive);
+
+        SmartDashboard.getNumber("kP Balance", kPBalance);
+        SmartDashboard.getNumber("kI Balance", kIBalance);
+        SmartDashboard.getNumber("kD Balance", kDBalance);
+
     }
 
     @Override
@@ -65,7 +88,7 @@ public class Balance extends CommandBase {
 
     /*@Override
     public void end() {
-        //stop drivetrain
+        //TODO: stop drivetrain
     }*/
 
 }
