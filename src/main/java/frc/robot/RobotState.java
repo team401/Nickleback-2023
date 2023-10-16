5f3d3c1f4da187f7150c644d132f26bb76bf8ca9
Minh Nguyen <minhnguyen@mcps.org>
2023-10-16 19:22:57 -0400

    rather meaningless balance for pos 2 sorry

--- src/main/java/frc/robot/RobotState.java
+++ src/main/java/frc/robot/RobotState.java
@@ -4,10 +4,13 @@ import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.kinematics.SwerveDriveOdometry;
 import edu.wpi.first.math.kinematics.SwerveModulePosition;
+<<<<<<< HEAD
 import edu.wpi.first.wpilibj.smartdashboard.Field2d;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj.smartdashboard.Field2d;
 import frc.robot.Constants.DriveConstants;
+=======
+>>>>>>> 6c4d1e4 (rather meaningless balance for pos 2 sorry)
 
 public class RobotState {
 
@@ -19,6 +22,7 @@ public class RobotState {
         return instance;
     }
 
+<<<<<<< HEAD
     private SwerveDriveOdometry odometry;
     private Field2d field = new Field2d();
 
@@ -29,11 +33,18 @@ public class RobotState {
     public void initializeOdometry(Rotation2d rotation, SwerveModulePosition[] modulePositions, Pose2d pose) {
         odometry = new SwerveDriveOdometry(DriveConstants.kinematics, rotation, modulePositions, pose);
 
+=======
+    SwerveDriveOdometry odometry;
+
+    public void initializeOdometry(Rotation2d rotation, SwerveModulePosition[] modulePositions, Pose2d initialPose) {
+        odometry = new SwerveDriveOdometry(null, rotation, modulePositions, initialPose);
+>>>>>>> 6c4d1e4 (rather meaningless balance for pos 2 sorry)
     }
 
     public void updateOdometry(Rotation2d rotation, SwerveModulePosition[] modulePositions) {
         odometry.update(rotation, modulePositions);
     }
+<<<<<<< HEAD
     
     public void resetOdometry(Rotation2d gyroAngle, SwerveModulePosition[] modulePositions, Pose2d pose) {
         odometry.resetPosition(gyroAngle, modulePositions, pose);
@@ -43,6 +54,10 @@ public class RobotState {
         field.setRobotPose(odometry.getPoseMeters());
         SmartDashboard.putData("Odometry Pose", field);
 
+=======
+
+    public Pose2d getOdometryFieldToRobot() {
+>>>>>>> 6c4d1e4 (rather meaningless balance for pos 2 sorry)
         return odometry.getPoseMeters();
     }
     
