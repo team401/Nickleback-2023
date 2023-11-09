2b4c73d18cc7f9f598ee45f3d141750311f75f68
Minh Nguyen <minhnguyen@mcps.org>
2023-11-08 19:21:53 -0500

    followpathwithevents + followpathholonomic

--- src/main/java/frc/robot/commands/auto/Auto.java
+++ src/main/java/frc/robot/commands/auto/Auto.java
@@ -3,18 +3,26 @@ package frc.robot.commands.auto;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.function.Consumer;
+import java.util.function.Supplier;
 
 import com.pathplanner.lib.path.PathConstraints;
 import com.pathplanner.lib.path.PathPlannerTrajectory;
+import com.pathplanner.lib.util.PIDConstants;
 import com.pathplanner.lib.util.PathPlannerLogging;
+import com.pathplanner.lib.util.ReplanningConfig;
 import com.pathplanner.lib.path.PathPlannerPath;
+import com.pathplanner.lib.commands.FollowPathHolonomic;
 import com.pathplanner.lib.commands.FollowPathWithEvents;
 import com.pathplanner.lib.commands.PathPlannerAuto;
 
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import frc.robot.RobotState;
 import frc.robot.subsystems.ArmSubsystem;
 import frc.robot.subsystems.ArmSubsystem.Mode;
 import frc.robot.subsystems.drive.DriveSubsystem;
@@ -40,32 +48,53 @@ public class Auto extends SequentialCommandGroup {
         
         PathPlannerPath pathGroup = PathPlannerPath.fromPathFile(pathName);
 
+        addCommands(followPath());
+
     }
 
 
-    public void periodic() {
-        
-    }
 
-
-    public void followPath() {
-    // This will load the file "Example Path.path" and generate it with a max velocity of 4 m/s and a max acceleration of 3 m/s^2
+    public FollowPathWithEvents followPath() {
         PathPlannerPath pathGroup = PathPlannerPath.fromPathFile(pathName);
 
-        // This is just an example event map. It would be better to have a constant, global event map
-        // in your code that will be used by all path following commands.
         HashMap<String, Command> eventMap = new HashMap<>();
         eventMap.put("ShootCube", new InstantCommand(() -> arm.setMode(Mode.SHOOT_HIGH)));
         eventMap.put("IntakeCube", new InstantCommand(() -> arm.setMode(Mode.INTAKE)));
 
-        FollowPathWithEvents command = new FollowPathWithEvents(
-            drive(pathGroup),
-            pathGroup,
-            eventMap    
+        Supplier<Pose2d> poseSupplier = () -> RobotState.getInstance().getOdometryFieldToRobot(); 
+        Supplier<ChassisSpeeds> speedsSupplier = () -> drive.getChassisSpeeds();
+        Consumer<ChassisSpeeds> outputRobotRelative = chassisSpeeds -> drive.setGoalChassisSpeeds(chassisSpeeds);
+
+
+        Command followPathHolonomic = new FollowPathHolonomic(
+            pathGroup, 
+            poseSupplier, 
+            speedsSupplier, 
+            outputRobotRelative,
+            new PIDConstants(0, 0, 0), 
+            new PIDConstants(0, 0, 0),
+            5.0,
+            0.1,
+            0.02,
+            new ReplanningConfig(),
+            drive
         );
+
+
+        FollowPathWithEvents command = new FollowPathWithEvents(
+            followPathHolonomic,
+            pathGroup,
+            poseSupplier  
+        );
+
+
+        return command;
+
     }
 
     
+
+
 
     
 }
