0c03385b71153072383edec12fda4cd5882e5e8d
Jack Lingle <captainjack0818@gmail.com>
2023-11-11 14:16:43 -0500

    add methods to read from  in simulation

--- src/main/java/frc/robot/subsystems/drive/SwerveModuleSim.java
+++ src/main/java/frc/robot/subsystems/drive/SwerveModuleSim.java
@@ -7,7 +7,7 @@ import edu.wpi.first.wpilibj.Timer;
 public class SwerveModuleSim {
     private final Timer time;
     private double timeSinceUpdate;
-    private double simulatedMotorPosition;
+    private double simulatedPosition;
     private double simulatedStateSpeed;
     private double lastTimeUpdated;
     private SwerveModuleState simulatedState;
@@ -17,17 +17,22 @@ public class SwerveModuleSim {
         lastTimeUpdated = time.get();
         // initialize state at 0
         simulatedState = new SwerveModuleState(0, Rotation2d.fromDegrees(0));
-        simulatedMotorPosition = 0;
+        simulatedPosition = 0;
         simulatedStateSpeed = 0;
         timeSinceUpdate = 0;
     }
+    public void updateStateAndPosition(SwerveModuleState desiredState) {
+        timeSinceUpdate = timer.get() - lastTime;
+        lastTime = timer.get();
 
-    public void setRotationVoltage(double volts) {
-        turnVolts = MathUtil.clamp(volts, -12.0, 12.0);
-        turnSim.setInputVoltage(turnVolts)
+        simulatedState = desiredState;
+        simulatedStateSpeed = simulatedState.speedMetersPerSecond;
+        simulatedPosition += (simulatedStateSpeed * timeSinceUpdate);
     }
-    public void setDriveVoltage(double volts) {
-        driveVolts = MathUtil.clamp(volts, -12.0, 12.0);
-        driveSim.setInputVoltage(driveVolts);
+    public SwerveModulePosition getPosition() {
+        return new SwerveModulePosition(simulatedPosition, simulatedState.angle);
+    }
+    public SwerveModuleState getState() {
+        return simulatedState;
     }
 }
